37b4fd1a77305bf24435d83017207c1990de4667
Murod <mambetovmurod80@gmail.com>
2020-11-28 18:04:08 +0500

    JS 1.0

---
+++ script.js
@@ -0,0 +1,37 @@
+var ism = prompt('Sizning ismingiz?')
+console.log('Mani ismim ' + ism);
+alert ('Mani ismim \n' + ism)
+
+
+var familiya = prompt('Sizning familyangiz?')
+console.log('Mani familiyam ' + familiya);
+alert ('Mani familiyam \n' + familiya)
+
+
+var yosh = prompt('Sizning yoshingiz?')
+console.log('Man ' + yosh + ' yoshdaman');
+alert ('Man ' + yosh + ' yoshdaman')
+
+
+alert ('Misollarni yeching...')
+
+
+var plus = prompt('Misol 1: 6 + 3 =')
+console.log('Misol 1: ' + '6 ' + '+ ' + '3 = ' + (6+3) + ', Sizning javob ' + plus);
+alert ('Misolning javobi ' + (6 + 3) + ', Sizning javobingiz ' + plus)
+
+var minus = prompt('Misol 2: 20 - 5 =')
+console.log('Misol 2: ' + '20 ' + '- ' + '5 = ' + (20-5) + ', Sizning javob ' + minus);
+alert ('Misolning javobi ' + (20 - 5) + ', Sizning javobingiz ' + minus)
+
+var astress = prompt('Misol 3: 15 * 2 =')
+console.log('Misol 3: ' + '15 ' + '* ' + '2 = ' + (15*2) + ', Sizning javob ' + astress);
+alert ('Misolning javobi ' + (15 * 2) + ', Sizning javobingiz ' + astress)
+
+var slash = prompt('Misol 4: 8 : 2 =')
+console.log('Misol 4: ' + '8 ' + ': ' + '2 = ' + (8/2) + ', Sizning javob ' + slash);
+alert ('Misolning javobi ' + (8 + 2) + ', Sizning javobingiz ' + slash)
+
+var residue = prompt('Misol 5: Qoldiqni toping 10 : 3 =')
+console.log('Misol 5: ' + '10 ' + ': ' + '3 = ' + 'Qoldiq ' + (10%3) + ', Sizning javob ' + residue);
+alert ('Misolning javobi ' + (10 % 3) + ', Sizning javobingiz ' + residue)
